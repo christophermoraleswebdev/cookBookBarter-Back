b0dcb874a17ba49c19e848c44c9ad099c024b302
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-24 12:36:50 -0500

    deployment configuration

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const url = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/cookBookBarter'
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/cookBookBarter')
