2d9d4ffa92686762c83c28fa94da93805e0c2977
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-07-27 09:29:03 -0500

    update to the user model

--- models/user.js
+++ models/user.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
       username: { 
             type: String, 
-            required: true, 
             unique: true 
       },
       email: { 
             type: String,
+            required: true, 
             unique: true, 
       },
       password: { 
